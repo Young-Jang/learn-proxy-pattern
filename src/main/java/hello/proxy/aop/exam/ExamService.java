b4a33ce1359bbf9a0ffdd075830968ff238fa4c2
b116104@naver.com <b116104@naver.com>
2023-04-10 14:03:27 +0900

    feat: learn aspect example

---
+++ src/main/java/hello/proxy/aop/exam/ExamService.java
@@ -0,0 +1,17 @@
+package hello.proxy.aop.exam;
+
+import hello.proxy.aop.exam.annotation.Trace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ExamService {
+
+    private final ExamRepository examRepository;
+
+    @Trace
+    public void request(String itemId) {
+        examRepository.save(itemId);
+    }
+}
